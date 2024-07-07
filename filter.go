68269e55dfda84eae8119fef909672d959b2dac0
PanGan21 <ganelis.pan@gmail.com>
2024-07-07 13:44:04 +0300

    add filter parsing

---
+++ filter.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+var filterMap = map[string]fsnotify.Op{
+	"CREATE": fsnotify.Create,
+	"WRITE":  fsnotify.Write,
+	"REMOVE": fsnotify.Remove,
+	"RENAME": fsnotify.Rename,
+	"CHMOD":  fsnotify.Chmod,
+}
+
+func parseFilters(filters []string) (fsnotify.Op, error) {
+	var op fsnotify.Op
+	for _, f := range filters {
+		event, exists := filterMap[strings.ToUpper(f)]
+		if !exists {
+			return 0, fmt.Errorf("invalid filter event: %s", f)
+		}
+		op |= event
+	}
+
+	return op, nil
+}
